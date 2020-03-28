40c3f05ea72c5a30f0d8b88cc32b679adf771dc6
Sidda Chalamalasetty <tosidda@gmail.com>
2020-03-28 12:23:16 -0400

    Updates

---
+++ precache-manifest.41f1c44d2a680a914c83d0cb42d37ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bba1581d7cd6a01c2c014cb57976466",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "760b353995f24df806b5",
+    "url": "/youtube-clone/static/js/2.0f6b6e8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.0f6b6e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d769ca36e3eac70604",
+    "url": "/youtube-clone/static/js/main.619b4b44.chunk.js"
+  },
+  {
+    "revision": "844d4cfab7860bee6e8b",
+    "url": "/youtube-clone/static/js/runtime-main.c0a9cebd.js"
+  }
+]);
